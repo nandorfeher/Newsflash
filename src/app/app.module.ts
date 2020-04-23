78cb0d0f5f3d14d98467ba4871494969fb63cd1a
Fehér Nándor <h757887@stud.u-szeged.hu>
2020-04-23 23:03:43 +0200

    Statikus fejléc hozzáadása a főoldalhoz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavbarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
